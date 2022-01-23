a5b71b20bc0e4dee6d81042951930e6732997fdc
paulweezydesign <weezy@paulweezydesign.com>
2022-01-22 19:16:18 -0700

    added ul

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(
+     <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+</ul>, document.getElementById("root"))
